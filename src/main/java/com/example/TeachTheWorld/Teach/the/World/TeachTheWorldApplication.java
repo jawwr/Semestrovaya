09cb599ac508e6b6ce4b2bca4bbc5d60308d9713
Arseny <20mineslend14@gmail.com>
2022-05-18 16:04:27 +0500

    начало

---
+++ src/main/java/com/example/TeachTheWorld/Teach/the/World/TeachTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeachTheWorld.Teach.the.World;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachTheWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeachTheWorld/Teach/the/World/TeachTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeachTheWorld.Teach.the.World;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
