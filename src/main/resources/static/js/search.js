47e96b6205f6da7bed796dfc8858e0cfeb826e4f
Arseny <20mineslend14@gmail.com>
2022-06-04 03:05:08 +0500

    сделан рабочий поиск

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,17 +1,60 @@
-document.title = document.location.search.split('=')[1] === ''
-    ? 'Поиск'
-    : decodeURI(document.location.search.split('=')[1]);
-
-new Vue({
+const search = new Vue({
     el: '#search',
     data(){
         return{
-            input: decodeURI(document.location.search.split('=')[1])
+            input: ''
         }
     },
+    mounted() {
+        this.input = decodeURI(document.location.search.split('=')[1]);
+    },
     methods: {
-        search: function(){
-            window.location.href = `/pages/search.html?q=${this.input}`
+        search: function(e){
+            courses.search(this.input)
         }
     }
-})
+});
+
+const courses = new Vue({
+    el: '#courses',
+    data() {
+        return{
+            info:['','','']
+        }
+    },
+    mounted(){
+        axios
+            .get(`/course/search?parameter=${search.input}`)
+            .then(response => {
+                this.info = response.data;
+            })
+            .catch(error => console.log(error));
+    },
+    methods: {
+        search: function (param){
+            axios
+                .get(`/course/search?parameter=${param}`)
+                .then(response => {
+                    this.info = response.data;
+                })
+                .catch(error => console.log(error));
+        }
+    }
+});
+
+Vue.component('course',{
+    props: ['course'],
+    template: '<div class="course">\n' +
+        '                <div class="title-image">' +
+        '                    <img :src="`${course.titlePhotoPath}`" class="course-image">' +
+        '                </div>\n' +
+        '                <a class="course-info">' +
+        '                    <a :href="`/pages/review.html?id=${course.id}`" class="course-title">' +
+        '                        {{course.title}}' +
+        '                    </a>' +
+        '                    <div class="course-description">\n' +
+        '                        {{course.description}}' +
+        '                    </div>' +
+        '                </div>' +
+        '            </div>'
+});
